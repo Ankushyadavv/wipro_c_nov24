7cbe44f12329eee4b8980adfe3c527f7503157cb
Ankushyadavv <ankush.1822ec1033@kiet.edu>
2024-11-22 23:19:33 +0530

    p9: Program to do formatting on string data

---
+++ Day2Task/FormattingOnStringData/p9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char place[16] = "bengaluru";
+    printf("%s\n", place);
+    printf("%12s\n", place);
+    printf("%-11s***\n", place);
+    printf("%.4s\n", place);
+    printf("%5s\n", place);
+    printf("%2.4s", place);
+}
